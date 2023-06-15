26a8695affcc46c09e38ea3881f4ea6ddb7f74e0
NikDjiev529 <134649365+NikDjiev529@users.noreply.github.com>
2023-06-15 08:31:16 +0300

    Update Zad1.java
    zavursh 1bu

--- Zad1.java
+++ Zad1.java
@@ -1,5 +1,7 @@
 package maturaZadachi;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -8,81 +10,76 @@ public class Zad1 {
 	public static void main(String[] args) {
 		LinkedList<Integer> spi = new LinkedList<Integer>();
 		for (int i = 0; i < 20; i++) {
-			spi.add(i);
+			spi.add(i-3);
 		}
-		System.out.println(prenaredi(spi));
-
+		char[] nizche = {'P','r','o','b','a','.','t','x','t'};
+		vurniChislata(nizche);
 	}
 
 	@SuppressWarnings("unused")
-	private static LinkedList<Integer> metodTrii(LinkedList<Integer> sp, int k) {//1а
+	private static LinkedList<Integer> metodTrii(LinkedList<Integer> sp, int k) {
 		Iterator<Integer> naSp = sp.iterator();
 		while (naSp.hasNext()) {
-			try {
 				if (sborZifri(naSp.next()) % k == 0) {
 					naSp.remove();
 				}
-			} catch (Exception E) {
-
-			}
 		}
+		
 		return sp;
 	}
 
-	private static int sborZifri(int a) {
+	private static int sborZifri(int a) {//chrez prevrushtane na otr v pol
 		int sbor = 0;
 		if(a<0) {
-			a=a*(-1);
+			a*=(-1);
 		}
 		while (a != 0) {
 			sbor += a % 10;
 			a = a / 10;
 		}
+		
 		return sbor;
 	}
 
-	private static LinkedList<Integer> prenaredi(LinkedList<Integer> sp){//1б
-		int []mas_sbor_Zifri = new int[sp.size()];
-		int []mas_lista = new int[sp.size()];
-		for(int i=0; i< sp.size(); i++) {
+	@SuppressWarnings("unused")
+	private static LinkedList<Integer> prenaredi(LinkedList<Integer> sp) {
+		int[] mas_sbor_Zifri = new int[sp.size()];
+		for (int i = 0; i < sp.size(); i++) {
 			mas_sbor_Zifri[i] = sborZifri(sp.get(i));
-			mas_lista[i] = sp.get(i);
 		}
-		for(int i=0; i< sp.size(); i++) {
-			System.out.print(mas_lista[i] + " ");		
+		int[] mas_lista_nareden = new int[sp.size()];
+		for (int i = 0; i < sp.size(); i++) {
+			mas_lista_nareden[i] = sp.get(i);
 		}
-		System.out.println();
 
 		int pom;
-		for(int j=0; j<sp.size() - 1; j++) {
-		for(int i=0; i< sp.size() - 1; i++) {
-			if(mas_sbor_Zifri[i]>mas_sbor_Zifri[i+1]) {
-				pom = mas_sbor_Zifri[i];
-				mas_sbor_Zifri[i] = mas_sbor_Zifri[i+1];
-				mas_sbor_Zifri[i+1] = pom;
-				pom = mas_lista[i];
-				mas_lista[i] = mas_lista[i+1];
-				mas_lista[i+1] = pom;
-				
+		for (int j = 1; j < sp.size(); j++) {
+			for (int i = 0; i < sp.size() - 1; i++) {
+				if (mas_sbor_Zifri[i] > mas_sbor_Zifri[i + 1]) {
+					pom = mas_sbor_Zifri[i];
+					mas_sbor_Zifri[i] = mas_sbor_Zifri[i + 1];
+					mas_sbor_Zifri[i + 1] = pom;
+
+					pom = mas_lista_nareden[i];
+					mas_lista_nareden[i] = mas_lista_nareden[i + 1];
+					mas_lista_nareden[i + 1] = pom;
+
+				}
 			}
 		}
-		}
+
 		sp.clear();
-		for(int i=0; i< mas_lista.length; i++) {
-			sp.add(mas_lista[i]);
+		for (int i = 0; i < mas_lista_nareden.length; i++) {
+			sp.add(mas_lista_nareden[i]);
 		}
-		
+
 		return sp;
 	}
-	
+	private static ArrayList<Integer> vurniChislata(char[] a){
+		ArrayList<Integer> spisuk = new ArrayList<Integer>();
+		File fileche = new File(a.toString());
+		
+		return spisuk;
+	}
 
 }
-//Resursi:
-/* for(int i=0; i< sp.size(); i++) {
-System.out.print(mas_lista[i] + " ");		
-}
-System.out.println();
-for(int i=0; i< sp.size(); i++) {
-System.out.print(mas_sbor_Zifri[i] + " ");
-}
-System.out.println(); */
