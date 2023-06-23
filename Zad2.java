0247e129fb7a814572b69346dfc28b46f4cc9728
NikDjiev529 <134649365+NikDjiev529@users.noreply.github.com>
2023-06-23 12:27:06 +0300

    Update Zad2.java
    almost done

--- Zad2.java
+++ Zad2.java
@@ -1,8 +1,8 @@
-package zadachiMatura;
+package maturaZadachi;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Zad2 {
@@ -12,13 +12,9 @@ public class Zad2 {
 		for(int i=0; i<5; i++) {
 			for(int j=0; j<6; j++) {
 				a[i][j] = 'a';
-				System.out.print(a[i][j] + " ");
 			}
-			System.out.println();
 		}
-		String buba = "bb";
-		contains(a, buba);
-
+		
 	}
 
 	private static int BroiElementiVRedVmatrizata(char[][] matr) {
@@ -43,7 +39,6 @@ public class Zad2 {
 			String sustavna = "";
 			for(int j=0; j<brElemVRed; j++) {
 				sustavna = sustavna + matriza[i][j];
-				System.out.println(sustavna);
 				if(sustavna.equals(duma)) {
 					return true;
 				}
@@ -54,7 +49,6 @@ public class Zad2 {
 			String sustavna2 = "";
 			for(int j=brElemVRed-1; j>=0; j--) {
 				sustavna2 = sustavna2 + matriza[i][j];
-				System.out.println(sustavna2);
 				if(sustavna2.equals(duma)) {
 					return true;
 				}
@@ -65,20 +59,84 @@ public class Zad2 {
 		return false;
 	}
 	
-	@SuppressWarnings("null")
+	
 	private static char[][] readMatrix(String imeFail) {
 		File failche = new File(imeFail);
 		Scanner minuvach = null;
 		try {
 			minuvach = new Scanner(failche);
 		} catch (FileNotFoundException e) {
-			minuvach.close();
 			System.out.println("File not found, bad, sorry.");
 			return null;
 		}
-		return null;
-
+		if(minuvach.hasNextLine() == false) {
+			minuvach.close();
+			//No content in the file
+			return null;
+		}
+		int broiSimvoliVPurviRed = (minuvach.nextLine()).length();
+		int broiRedove = 1;
+		if(minuvach.hasNextLine() == false && broiSimvoliVPurviRed != 1) {
+			//Invalid matrix, more than 1 symbols in first line
+			minuvach.close();
+			return null;
+		}
+		while(minuvach.hasNextLine()) {
+			if(broiSimvoliVPurviRed != (minuvach.nextLine()).length() ) {
+				//Invalid matrix, not rectangular
+				minuvach.close();
+				return null;
+			}
+			broiRedove++;
+		}
+		minuvach.close();
+		char[][] matrizaFail = new char[broiRedove][broiSimvoliVPurviRed];
+		Scanner minuvachVtori = null;
+		try {
+			minuvachVtori = new Scanner(failche);
+		} catch (FileNotFoundException e) {
+			System.out.println("Bez znachenie");//Tazi proverka veche e izvurshena po-gore, no se iziskva pak da se napravi.
+		}
+		int redNaKoitoSme=0;
+		while(minuvachVtori.hasNextLine()) {
+			String redOtFaila = minuvachVtori.nextLine();
+			char[] redOtFailaDa = redOtFaila.toCharArray();
+			for(int i=0; i<redOtFaila.length(); i++) {
+				matrizaFail[redNaKoitoSme][i] = redOtFailaDa[i];
+			}
+			redNaKoitoSme++;
+		}
+		
+		return matrizaFail;
 	}
+	
+	private static LinkedList<String> readWords(String imeFail){
+		File failche = new File(imeFail);
+		Scanner minuvach = null;
+		try {
+			minuvach = new Scanner(failche);
+		}catch(FileNotFoundException e) {
+			System.out.println("File not found, sorry :|");
+			return null;
+		}
+		LinkedList <String> spisuka = new LinkedList<String>();
+		while(minuvach.hasNextLine()) {
+			spisuka.add(minuvach.nextLine());
+		}
+		
+		minuvach.close();
+		return spisuka;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
 
